b51c17fb27d64ae896190c04f35877f34e0eede6
malloyethan11 <40256124+malloyethan11@users.noreply.github.com>
2021-06-21 16:33:46 -0400

    Adds backend functionality to frmAddVendor. This includes basic input validation.

--- CashRegister/frmAddVendor.vb
+++ CashRegister/frmAddVendor.vb
@@ -1,4 +1,7 @@
-﻿Public Class frmAddVendor
+﻿
+' Database code adapted from Programming 2 homework
+
+Public Class frmAddVendor
     Private Sub frmAddVendor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         Me.CenterToScreen()
@@ -15,9 +18,91 @@
 
     End Sub
 
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        ' declare variables
+        Dim strVendor As String = ""
+        Dim strDbInsertString As String = ""
+        Dim cmdInsert As OleDb.OleDbCommand
+
+        ' get value from user entry
+        strVendor = txtVendorName.Text
+
+        ' validate user input
+        If ValidateInput(strVendor) = True Then
+
+            Try
+
+                ' open the DB
+                If OpenDatabaseConnectionSQLServer() = False Then
+
+                    ' No, warn the user ...
+                    MessageBox.Show(Me, "Database connection error." & vbNewLine &
+                                            "The application will now close.",
+                                            Me.Text + " Error",
+                                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                    ' and close the form/application
+                    Me.Close()
+
+                End If
+
+                ' build db insert string and command
+                strDbInsertString = "INSERT INTO TVendors (strVendorName) VALUES ('" & strVendor & "');"
+                cmdInsert = New OleDb.OleDbCommand(strDbInsertString, m_conAdministrator)
+
+                ' insert the records
+                cmdInsert.ExecuteNonQuery()
+
+                ' close the db connection
+                CloseDatabaseConnection()
+
+                ' display message box to notify of successful insert
+                MessageBox.Show("Vendor successfully added!")
+
+                ' clear textbox for next vendor
+                txtVendorName.Clear()
+
+
+            Catch excError As Exception
+
+                ' log and display error message
+                MessageBox.Show(excError.Message)
+
+            End Try
+
+        End If
+
+
+    End Sub
+
+
+    Private Function ValidateInput(ByVal strInput As String) As Boolean
+
+        ' declare variable
+        Dim blnResult As Boolean
+
+        If strInput = "" Then
+
+            MessageBox.Show("Please enter a vendor name.")
+
+        Else
+
+            blnResult = True
+
+        End If
+
+        Return blnResult
+
+    End Function
+
+
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
 
         OpenFormKillParent(Me, frmInventory)
 
     End Sub
+
+
 End Class

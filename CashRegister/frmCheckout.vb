d984aeb5fbb9696a515ed11fcc4bf13819095298
zmaljloob <86270190+zmaljloob@users.noreply.github.com>
2021-07-09 22:38:17 +0530

    Some Problems with lookup otherwise its complete. Will add validation later

--- CashRegister/frmCheckout.vb
+++ CashRegister/frmCheckout.vb
@@ -1,4 +1,6 @@
-﻿Public Class frmCheckout
+﻿Imports System.Data.OleDb
+
+Public Class frmCheckout
 
     Private Sub frmCheckout_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -49,4 +51,73 @@
 
     End Sub
 
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim roller As ComboBox = sender
+        If roller.SelectedIndex = 2 Then
+            TextBox5.Enabled = True
+            TextBox6.Enabled = True
+            dtpExpirationDate.Enabled = True
+        Else
+            TextBox5.Enabled = False
+            TextBox6.Enabled = False
+            dtpExpirationDate.Enabled = False
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+        If IsNumeric(TextBox1.Text) Then
+            TextBox2.Text = Convert.ToDecimal(TextBox1.Text) * 0.575
+        Else
+            TextBox2.Text = "NAN"
+        End If
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Try
+            ' Open the DB
+            If OpenDatabaseConnectionSQLServer() = False Then
+
+                ' The database is not open
+                MessageBox.Show(Me, "Database connection error." & vbNewLine &
+                                "The form will now close.",
+                                Me.Text + " Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                ' Close the form/application
+                Me.Close()
+
+            End If
+            Dim cmdInsert = New OleDbCommand("INSERT INTO TTransactions(intTransactionTypeID, intPaymentTypeID, strFirstName, strLastName, strAddress, strCity, intStateID, strZip, strPhoneNumber, strEmail, strCreditCard, strExpirationDate, strSecurityCode, decTotalPrice, decSalesTax, strDescription, strUserName) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
+            cmdInsert.CommandType = CommandType.Text
+            cmdInsert.Connection = m_conAdministrator
+            ' Username Password
+            cmdInsert.Parameters.AddWithValue("intTransactionTypeID", 1)
+            cmdInsert.Parameters.AddWithValue("intPaymentTypeID", ComboBox1.SelectedIndex)
+            ' UserData
+            cmdInsert.Parameters.AddWithValue("strFirstName", TextBox3.Text)
+            cmdInsert.Parameters.AddWithValue("strLastName", TextBox4.Text)
+            cmdInsert.Parameters.AddWithValue("strAddress", TextBox9.Text)
+            cmdInsert.Parameters.AddWithValue("strCity", TextBox7.Text)
+            cmdInsert.Parameters.AddWithValue("intStateID", cboState.SelectedIndex)
+            cmdInsert.Parameters.AddWithValue("strZip", TextBox8.Text)
+            cmdInsert.Parameters.AddWithValue("strPhoneNumber", txtPhoneNumber.Text)
+            cmdInsert.Parameters.AddWithValue("strEmail", txtEmail.Text)
+            ' Credit Card
+            cmdInsert.Parameters.AddWithValue("strCreditCard", TextBox5.Text)
+            cmdInsert.Parameters.AddWithValue("strExpirationDate", dtpExpirationDate.Value)
+            cmdInsert.Parameters.AddWithValue("strSecurityCode", TextBox6.Text)
+
+            cmdInsert.Parameters.AddWithValue("decTotalPrice", Convert.ToDecimal(TextBox1.Text))
+            cmdInsert.Parameters.AddWithValue("decSalesTax", Convert.ToDecimal(TextBox2.Text))
+
+            cmdInsert.Parameters.AddWithValue("strDescription", "?")
+            cmdInsert.Parameters.AddWithValue("strUserName", "")
+            Dim result = cmdInsert.ExecuteNonQuery()
+            ' If result is one that means a row is added
+            MessageBox.Show(result.ToString + " Purchase Added successfully")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
 End Class
--- CashRegister/frmReturn.vb
+++ CashRegister/frmReturn.vb
@@ -1,4 +1,6 @@
-﻿Public Class frmReturn
+﻿Imports System.Data.OleDb
+
+Public Class frmReturn
 
     Private Sub frmReturn_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -48,4 +50,88 @@
 
     End Sub
 
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim roller As ComboBox = sender
+        If roller.SelectedIndex = 2 Then
+            TextBox5.Enabled = True
+            TextBox6.Enabled = True
+            dtpExpirationDate.Enabled = True
+        Else
+            TextBox5.Enabled = False
+            TextBox6.Enabled = False
+            dtpExpirationDate.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub txtReturn_TextChanged(sender As Object, e As EventArgs) Handles txtReturn.TextChanged
+        If IsNumeric(txtReturn.Text) Then
+            txtSalesTax.Text = Convert.ToDecimal(txtReturn.Text) * 0.575
+        Else
+            txtSalesTax.Text = "NAN"
+        End If
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        ' Validations
+        If TextBox3.Text = "" Or ' For credit not added
+                TextBox4.Text = "" Or 'TextBox5.Text = "" Or TextBox6.Text = "" Or
+                TextBox7.Text = "" Or
+                TextBox8.Text = "" Or
+                txtEmail.Text = "" Or
+                txtPhoneNumber.Text = "" Or
+                txtReturn.Text = "" Or
+                txtSalesTax.Text = "" Or
+                TextBox9.Text = "" Then
+
+            ' Some Things to show Error not making it compulsory as cash or credit will differ
+        Else
+            Try
+                ' Open the DB
+                If OpenDatabaseConnectionSQLServer() = False Then
+
+                    ' The database is not open
+                    MessageBox.Show(Me, "Database connection error." & vbNewLine &
+                                    "The form will now close.",
+                                    Me.Text + " Error",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                    ' Close the form/application
+                    Me.Close()
+
+                End If
+                Dim cmdInsert = New OleDbCommand("INSERT INTO TTransactions(intTransactionTypeID, intPaymentTypeID, strFirstName, strLastName, strAddress, strCity, intStateID, strZip, strPhoneNumber, strEmail, strCreditCard, strExpirationDate, strSecurityCode, decTotalPrice, decSalesTax, strDescription, strUserName) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
+                cmdInsert.CommandType = CommandType.Text
+                cmdInsert.Connection = m_conAdministrator
+                ' Username Password
+                cmdInsert.Parameters.AddWithValue("intTransactionTypeID", 2)
+                cmdInsert.Parameters.AddWithValue("intPaymentTypeID", ComboBox1.SelectedIndex)
+                ' UserData
+                cmdInsert.Parameters.AddWithValue("strFirstName", TextBox3.Text)
+                cmdInsert.Parameters.AddWithValue("strLastName", TextBox4.Text)
+                cmdInsert.Parameters.AddWithValue("strAddress", TextBox9.Text)
+                cmdInsert.Parameters.AddWithValue("strCity", TextBox7.Text)
+                cmdInsert.Parameters.AddWithValue("intStateID", cboState.SelectedIndex)
+                cmdInsert.Parameters.AddWithValue("strZip", TextBox8.Text)
+                cmdInsert.Parameters.AddWithValue("strPhoneNumber", txtPhoneNumber.Text)
+                cmdInsert.Parameters.AddWithValue("strEmail", txtEmail.Text)
+                ' Credit Card
+                cmdInsert.Parameters.AddWithValue("strCreditCard", TextBox5.Text)
+                cmdInsert.Parameters.AddWithValue("strExpirationDate", dtpExpirationDate.Value)
+                cmdInsert.Parameters.AddWithValue("strSecurityCode", TextBox6.Text)
+
+                cmdInsert.Parameters.AddWithValue("decTotalPrice", Convert.ToDecimal(txtReturn.Text))
+                cmdInsert.Parameters.AddWithValue("decSalesTax", txtSalesTax.Text)
+
+                cmdInsert.Parameters.AddWithValue("strDescription", "?")
+                cmdInsert.Parameters.AddWithValue("strUserName", "")
+                Dim result = cmdInsert.ExecuteNonQuery()
+                ' If result is one that means a row is added
+                MessageBox.Show(result.ToString + "Returns Added successfully")
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        End If
+
+    End Sub
 End Class
